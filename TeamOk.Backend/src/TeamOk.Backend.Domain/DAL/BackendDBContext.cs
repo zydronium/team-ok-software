46e1de18cb47a65ba7f098e2de4809a776ac1c62
Jelle Luteijn <git@jelleluteijn.pro>
2017-04-11 12:55:53 +0200

    db context for backend

---
+++ TeamOk.Backend/src/TeamOk.Backend.Domain/DAL/BackendDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamOk.Backend.Domain.DAL
+{
+    public class BackendDBContext : DbContext
+    {
+        public BackendDBContext(DbContextOptions<BackendDBContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
