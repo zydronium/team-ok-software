f17e2030ea3821401f85f0d609f6c3bf31a78423
Dyon <dyonunic@gmail.com>
2017-04-12 10:44:43 +0200

    Mystuff

--- TeamOk.WorkFrontend/src/TeamOk.WorkFrontend.Facade/Controllers/KioskController.cs
+++ TeamOk.WorkFrontend/src/TeamOk.WorkFrontend.Facade/Controllers/KioskController.cs
@@ -10,17 +10,27 @@ namespace TeamOk.WorkFrontend.Facade.Controllers
     {
         public IActionResult Index()
         {
-            //Vraag de backend wat de status is van deze tafel 
+           
+            var Bezet = getIsBezet();
 
             //If tafel = bezet
             //return View("Bezet");
-            
-
             //If tafel != bezet
             //Return View("Vrij")
+            if (Bezet)
+            {
+                return View("Bezet");
+            }
+            else
+            {
+                return View("Vrij");
+            }
+        }
 
-
-            return View();
+        private bool getIsBezet()
+        {
+            //Vraag de backend wat de status is van deze tafel 
+            return false;
         }
 
         public IActionResult About()
