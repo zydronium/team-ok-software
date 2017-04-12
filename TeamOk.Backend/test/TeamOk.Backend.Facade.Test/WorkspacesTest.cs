c36d85658d71542bcecaf34f079bbad1a2974782
Jelle Luteijn <git@jelleluteijn.pro>
2017-04-12 11:30:03 +0200

    add unittests

---
+++ TeamOk.Backend/test/TeamOk.Backend.Facade.Test/WorkspacesTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamOk.Backend.Facade.Test
+{
+    [TestClass]
+    public class WorkspacesTest
+    {
+    }
+}
