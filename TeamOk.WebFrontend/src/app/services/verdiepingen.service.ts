33529145411496b90921f7fcb6839d9b45067f53
Tessa den Oudsten <tessa.denoudsten@student.hu.nl>
2017-04-12 12:14:49 +0200

    New style for the tables (locaties & verdiepingen etc.)

--- TeamOk.WebFrontend/src/app/services/verdiepingen.service.ts
+++ TeamOk.WebFrontend/src/app/services/verdiepingen.service.ts
@@ -13,6 +13,8 @@ export class VerdiepingenService {
       {id: 3, naam: "Instituutsplein ICT", werkplekken: 42, vrije_werkplekken: 0, locatieid: 1},
       {id: 4, naam: "Verdieping 3", werkplekken: 11, vrije_werkplekken: 4, locatieid: 1},
       {id: 5, naam: "Verdieping 4", werkplekken: 16, vrije_werkplekken: 6, locatieid: 1},
+      {id: 51, naam: "Verdieping 5", werkplekken: 16, vrije_werkplekken: 6, locatieid: 1},
+      {id: 52, naam: "Verdieping 6", werkplekken: 16, vrije_werkplekken: 6, locatieid: 1},
       {id: 6, naam: "Bagane grond", werkplekken: 15, vrije_werkplekken: 5, locatieid: 2},
       {id: 7, naam: "Verdieping 1", werkplekken: 20, vrije_werkplekken: 4, locatieid: 2},
       {id: 8, naam: "Verdieping 2", werkplekken: 14, vrije_werkplekken: 0, locatieid: 2},
