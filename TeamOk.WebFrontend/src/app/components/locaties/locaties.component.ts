c5d393746f645b29c06acd5f0e712ce36a635199
Merel de Klein <mereldeklein@live.nl>
2017-04-13 09:59:49 +0200

    arrows in list locations. pointer now changes when on list item.

--- TeamOk.WebFrontend/src/app/components/locaties/locaties.component.ts
+++ TeamOk.WebFrontend/src/app/components/locaties/locaties.component.ts
@@ -16,7 +16,7 @@ locaties: any;
   ngOnInit() {
     this.locaties = this.locatiesService.getLocaties();
   }
-  
+
   openLocatie(locatie){
       // this.locatieService.setLocatie(locatie);
       this.router.navigate(['/locaties/'+ locatie.id]);
