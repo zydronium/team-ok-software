cf8eb1a3eea032c08cd9fb46e3a4291980391e80
Merel de Klein <mereldeklein@live.nl>
2017-04-13 11:34:27 +0200

    werkplek ohalen objecten. breadcrumbs.

--- TeamOk.WebFrontend/src/app/components/werkplek/werkplek.component.ts
+++ TeamOk.WebFrontend/src/app/components/werkplek/werkplek.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {Locatie} from "../../models/Locatie";
+import {Verdieping} from "../../models/Verdieping";
+import {Werkplek} from "../../models/Werkplek";
+import {ActivatedRoute, Router} from "@angular/router";
+import {LocatiesService} from "../../services/locaties.service";
+import {VerdiepingenService} from "../../services/verdiepingen.service";
+import {WerkplekkenService} from "../../services/werkplekken.service";
 
 @Component({
   selector: 'app-werkplek',
@@ -6,10 +13,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./werkplek.component.css']
 })
 export class WerkplekComponent implements OnInit {
+  locatieid: number;
+  verdiepingid: number;
+  werkplekid: number;
+  locatie: Locatie;
+  verdieping: Verdieping;
+  werkplek: Werkplek;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private locatiesService: LocatiesService,
+    private verdiepingenService : VerdiepingenService,
+    private werkplekkenService: WerkplekkenService
+  ) {
+
+  }
 
   ngOnInit() {
+    this.locatieid = this.route.snapshot.params['locatieid'];
+    this.locatie = this.locatiesService.getLocatie(this.locatieid);
+
+    this.verdiepingid = this.route.snapshot.params["verdiepingid"];
+    this.verdieping = this.verdiepingenService.getVerdieping(this.verdiepingid);
+
+    this.werkplek = this.werkplekkenService.getWerkplek(this.verdiepingid);
   }
 
 }
