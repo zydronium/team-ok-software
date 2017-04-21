52a3b43eef742a17fadc56ea3bb852306ed7a4b2
Arjan van Maren <Arjanvanmaren@gmail.com>
2017-04-21 11:27:18 +0200

    Tijd kiezen gefixt met cookie, reserveer werkplek gefixt met cookie....

--- TeamOk.WorkFrontend/src/TeamOk.WorkFrontend.Facade/Controllers/KioskController.cs
+++ TeamOk.WorkFrontend/src/TeamOk.WorkFrontend.Facade/Controllers/KioskController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TeamOk.WorkFrontend.Facade.Models;
 using TeamOk.WorkFrontend.Facade;
+using Microsoft.AspNetCore.Http;
 
 namespace TeamOk.WorkFrontend.Facade.Controllers
 {
@@ -46,9 +47,17 @@ namespace TeamOk.WorkFrontend.Facade.Controllers
         }
 
         [HttpPost]
-        public void reserveerWerkplek(object sender, EventArgs e)
+        public void reserveerWerkplek(object sender, EventArgs e, [FromQuery] String MacAddress)
         {
-            Console.Write("Hi");
+            if (!getIsBezet(MacAddress))
+            {
+                int Minutes = Convert.ToInt32(Request.Cookies["ChosenMinutes"]);
+                int Hours = Convert.ToInt32(Request.Cookies["ChosenHours"]);
+                Status status = new Status();
+                status.ClaimedUntill = DateTime.Now.AddMinutes(Minutes).AddHours(Hours);
+                status.Claimed = true;
+                _context.ApiWorkspaceunitsByMacAddressPost(MacAddress, status);
+            }
         }
 
         public IActionResult About()
@@ -76,11 +85,17 @@ namespace TeamOk.WorkFrontend.Facade.Controllers
         }
 
         [HttpPost]
-        public ActionResult ChooseTime(ChosenTimeModel model)
+        public ActionResult ChooseTime(ChosenTimeModel model, [FromQuery] String MacAddress)
         {
             int hours = model.Hours;
             int minutes = model.Minutes;
-            if (getIsBezet(""))
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddMinutes(10);
+            string s = model.ToString();
+            Response.Cookies.Append("ChosenMinutes", hours.ToString(), options);
+            Response.Cookies.Append("ChosenHours", minutes.ToString(), options);
+
+            if (getIsBezet(MacAddress))
             {
                 return View("Bezet");
             }
