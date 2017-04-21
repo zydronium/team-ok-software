22cadb37b273cdbed97e182944b3a4b59e0fde6a
Dyon <dyonunic@gmail.com>
2017-04-21 11:40:14 +0200

    Revert "no changes"
    This reverts commit ffc0c05aed8ada5f81ea685fa10042dc85a6ce41.

---
+++ TeamOk.WorkFrontend/src/TeamOk.WorkFrontend.Facade/Controllers/KioskController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TeamOk.WorkFrontend.Facade.Models;
+using TeamOk.WorkFrontend.Facade;
+using Microsoft.AspNetCore.Http;
+
+namespace TeamOk.WorkFrontend.Facade.Controllers
+{
+    public class KioskController : Controller
+    {
+
+        private readonly IWerkplekkenBackend _context;
+
+        public KioskController(IWerkplekkenBackend context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index([FromHeader]string macaddress, [FromQuery] string MAC)
+        {
+            Console.Write("Hello Index");
+            var Bezet = getIsBezet(MAC);
+            Console.Write(MAC);
+            var test = _context.ApiWorkspaceunitsByMacAddressGet("test");
+            //If tafel = bezet
+            //return View("Bezet");
+            //If tafel != bezet
+            //Return View("Vrij")
+
+            if (Bezet)
+            {
+                return View("Bezet");
+            }
+            else
+            {
+                return View("Vrij");
+            }
+        }
+
+        private bool getIsBezet(string mac)
+        {
+            //Vraag de backend wat de status is van deze tafel 
+            return false;
+        }
+
+        [HttpPost]
+        public void reserveerWerkplek(object sender, EventArgs e, [FromQuery] String MacAddress)
+        {
+            if (!getIsBezet(MacAddress))
+            {
+                double? Minutes = Convert.ToDouble(Request.Cookies["ChosenMinutes"]);
+                double? Hours = Convert.ToDouble(Request.Cookies["ChosenHours"]);
+                if (Minutes != null && Hours != null)
+                {
+                    Status status = new Status();
+                    status.ClaimedUntill = DateTime.Now.AddMinutes((double) Minutes).AddHours((double) Hours);
+                    status.Claimed = true;
+                    _context.ApiWorkspaceunitsByMacAddressPost(MacAddress, status);
+                }
+                else
+                {
+                    Status status = new Status();
+                    status.ClaimedUntill = DateTime.Now.AddMinutes(30).AddHours(0);
+                    status.Claimed = true;
+                    _context.ApiWorkspaceunitsByMacAddressPost(MacAddress, status);
+                }
+            }
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Voor meer info";
+            ViewData["URL"] = "bit.ly/????";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Timer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ChooseTime(ChosenTimeModel model, [FromQuery] String MacAddress)
+        {
+            int hours = model.Hours;
+            int minutes = model.Minutes;
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddMinutes(10);
+            string s = model.ToString();
+            Response.Cookies.Append("ChosenMinutes", hours.ToString(), options);
+            Response.Cookies.Append("ChosenHours", minutes.ToString(), options);
+
+            if (getIsBezet(MacAddress))
+            {
+                return View("Bezet");
+            }
+            else return View("Vrij");
+        }
+
+    }
+}
