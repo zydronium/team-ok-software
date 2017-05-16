ddd86268e3b3c9c296d4afce91939da82f451369
Danny Deelen <Danny888deelen@gmail.com>
2017-05-16 12:21:52 +0200

    Begin van de zoekfunctie

--- TeamOk.WebFrontend/src/app/components/zoek/zoek.component.ts
+++ TeamOk.WebFrontend/src/app/components/zoek/zoek.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {WerkplekkenService} from "../../services/werkplekken.service";
+import {Werkplek} from "../../models/Werkplek";
 
 @Component({
   selector: 'app-zoek',
@@ -7,13 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ZoekComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private werkplekkenService: WerkplekkenService) { }
+  werkplekken: any = [];
+  vrijeWerkplekken: any = [];
+  result: any = [];
+  zitplaatsen: number = 1;
+  stopcontacten: number = 1;
   ngOnInit() {
   }
 
-  zoek()
-  {
+  zoek() {
+    this.werkplekken = this.werkplekkenService.getAlleWerkplekken();
+    for(var i =0; i < this.werkplekken.length; i++){
+      if(this.werkplekken[i].claimed == false){
+        this.werkplekken[i].seat = 1;
+        this.vrijeWerkplekken.push(this.werkplekken[i]);
+      }
+    }
+    for(var i =0; i < this.vrijeWerkplekken.length; i++){
+      if(this.vrijeWerkplekken[i].stop == false){
+        this.vrijeWerkplekken.push(this.werkplekken[i]);
+      }
+    }
     console.log("zoek");
   }
 }
