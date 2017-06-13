a1074d12e1ec17b0f0853d80bf0b68896d7f93b1
Jelle Luteijn <git@jelleluteijn.pro>
2017-06-13 11:35:13 +0200

    basic database for unit testing

--- TeamOk.Backend/test/TeamOk.Backend.Facade.Test/NotificationsTest.cs
+++ TeamOk.Backend/test/TeamOk.Backend.Facade.Test/NotificationsTest.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TeamOk.Backend.Domain.DAL;
 
 namespace TeamOk.Backend.Facade.Test
 {
     [TestClass]
     public class NotificationsTest
     {
+        private static DbContextOptions<BackendDBContext> CreateNewContextOptions()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+            var builder = new DbContextOptionsBuilder<BackendDBContext>();
+            builder.UseInMemoryDatabase()
+                .UseInternalServiceProvider(serviceProvider);
+            return builder.Options;
+        }
     }
 }
